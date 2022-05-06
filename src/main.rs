2bf130ea6500fcefdaffdd7cfc865bc95f86a4f6
adwai <1nclude@riseup.net>
2022-05-06 19:49:39 +0300

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    println!("Please input yout guess.");
+
     let mut guess = String::new();
+
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line");
+        .expect("Failed to load read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match  guess.cmp(&secret_number){
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
